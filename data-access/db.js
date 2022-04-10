b7d25ebb9c4a50278c5990b88250cfce3178ce11
jaycobobob <jacobtheedwards@gmail.com>
2022-04-09 18:12:21 -0700

    Integrated Postgresql Database

--- data-access/db.js
+++ data-access/db.js
@@ -1,12 +1,32 @@
 const bcrypt = require("bcrypt");
+const res = require("express/lib/response");
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const connectionString = `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`;
-
+const connectionString = `postgress://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_DATABASEPORT}/${process.env.DB_DATABASE}`;
 const connection = {
-    connectionString: process.env.DATABASE_URL
-        ? process.env.DATABASE_URL
-        : connectionString,
+    connectionString: process.env.DATABASE_URL ? process.env.DATABASEURL : connectionString,
     ssl: { rejectUnauthorized: false },
 };
 const pool = new Pool(connection);
+let store = {
+    addCustomer: (name, email, password) => {
+        let hash = bcrypt.hashSync(password, 10);
+        return pool.query(
+            "insert into imagequiz.customer (name, email, password) values ($1 , $2 , $3)",
+            [name, email, hash]
+        );
+    },
+
+    login: (email, password) => {
+        return pool.query("select name, email, password from imagequiz.customer where email = $1", [
+            email,
+        ]);
+    },
+
+    getFlowers: () => {
+        return pool.query("select * from imagequiz.flowers");
+    },
+};
+
+module.exports = { store };
--- index.js
+++ index.js
@@ -1,107 +1,176 @@
 const express = require("express");
-const cors = require("cors");
-const { customers } = require("./data-access/data/customers");
-const { flowers } = require("./data-access/data/flowers");
-const { quizzes } = require("./data-access/data/data");
-const { scores } = require("./data-access/data/scores");
+const bcrypt = require("bcrypt");
+const { store } = require("./data-access/db");
+var cors = require("cors");
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Creates the express server.
+const application = express();
 
-app.use(cors());
-app.use(express.json());
+// Middlewares process the requests before they get sent to our request handlers
+// Initialize them here
+application.use(cors());
+application.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).json({ done: true, message: "Fine!" });
-});
-
-app.post("/register", (req, res) => {
-    let customer = customers.find((c) => c.email === req.body.email);
-    if (!customer) {
-        customer = {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        };
-        customers.push(customer);
-        res.status(200).json({
-            done: true,
-            message: "Account registered successfully.",
+// Setting up request handlers for various api calls
+application.post("/register", (req, res) => {
+    /*
+     * POST method that stores the name, email and password of the quiz taker.
+     * If a customer with the same email already exists, return a response with
+     * the status code 403. The method should return an object {done, message}
+     * where done is boolean and message is a string;
+     */
+    store
+        .addCustomer(req.body.name, req.body.email, req.body.password)
+        .then((x) => {
+            res.status(200).json({
+                done: true,
+                message: "Account created successfully.",
+            });
+        })
+        .catch((e) => {
+            console.log(e);
+            res.status(400).json({
+                done: false,
+                message: "The customer was not added due to an error.",
+            });
         });
-    } else {
-        res.status(403).json({ done: false, message: "Email already exists." });
-    }
 });
 
-app.post("/login", (req, res) => {
-    let customer = customers.find((c) => c.email === req.body.email);
-    if (!customer) {
-        res.status(200).json({ done: false, message: "Email does not exist." });
-    } else if (customer && customer.password !== req.body.password) {
-        res.status(200).json({ done: false, message: "Password incorrect." });
-    } else {
-        res.status(200).json({ done: true, message: "Credentials valid." });
-    }
+application.post("/login", (req, res) => {
+    /*
+     * POST method that determinse whether the provided credentials
+     * (email, password) are valid or not. The method should return an object
+     * {done, message}.
+     */
+    store
+        .login(req.body.email, req.body.password)
+        .then((x) => {
+            if (x.rows.length === 1) {
+                let user = x.rows[0];
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    res.status(200).json({
+                        done: true,
+                    });
+                } else {
+                    res.status(400).json({
+                        done: false,
+                        message: "Invalid credentials.",
+                    });
+                }
+            } else {
+                res.status(400).json({
+                    done: false,
+                    message: "The specified email could not be found.",
+                });
+            }
+        })
+        .catch((e) => {
+            console.error(e);
+            res.status(500).json({
+                done: false,
+                message: "The credientials could not be validated due to an error.",
+            });
+        });
 });
 
-app.get("/flowers", (req, res) => {
+application.post("/score", (req, res) => {
+    /*
+     * POST method storing the score for a quiz taken by a quiz taker. The method
+     * sends an object {quizTaker, quizName, score}. The method should return an
+     * object {done, message}.
+     */
+    res.status(400).json({
+        done: false,
+        message: "This method is not implemented yet.",
+    });
+});
+
+application.get("/flowers", (req, res) => {
+    /*
+     * GET method returning the list of all the flowers. The method should return
+     * an object {done, result, message} where result is an array.
+     */
+    store
+        .getFlowers()
+        .then((x) => {
+            res.status(200).json({ done: true, result: x.rows });
+        })
+        .catch((e) => {
+            res.status(500).json({
+                done: false,
+                result: [],
+                message: "The data could not be retrieved due to an error.",
+            });
+        });
+});
+
+application.get("/quiz/:id", (req, res) => {
+    /*
+     * GET method returning the specified quiz. The method should return an object
+     * {done, result, message} where result is an object or it is undefined.
+     */
+
+    store
+        .getQuiz(req.params.id)
+        .then((x) => {
+            if (x.rows.length == 1) {
+                res.status(200).json({ done: true, result: x.rows[0] });
+            } else {
+                res.status(400).json({
+                    done: false,
+                    result: undefined,
+                    message: "Could not find a quiz for that user.",
+                });
+            }
+        })
+        .catch((e) => {
+            res.status(500).json({
+                done: false,
+                result: [],
+                message: "The data could not be retrieved due to an error.",
+            });
+        });
+});
+
+application.get("/scores/:quiztaker/:quizname", (req, res) => {
+    /*
+     * GET method returning the scores of the specified quiz taker for the
+     * specified quiz. The method should return an object {done, results, message}
+     * where results is an array or it is undefined.
+     */
+    store
+        .getScore(req.params.quiztaker, req.params.quizname)
+        .then((x) => {
+            if (x.rows.length > 0) {
+                res.status(200).json({ done: true, result: x.rows });
+            } else {
+                res.status(400).json({ done: false, message: "No scores found." });
+            }
+        })
+        .catch((e) => {
+            res.status(500).json({
+                done: false,
+                result: [],
+                message: "The data could not be retrieved due to an error.",
+            });
+        });
+});
+
+application.get("/", (req, res) => {
+    /*
+     * GET method returning a server status message. The method should return an
+     * object {status, message}
+     */
     res.status(200).json({
         done: true,
-        result: flowers,
-        message: "Flower data retrieved successfully",
+        message: "Connected to the imagequiz api",
     });
 });
 
-app.get("/quiz/:id", (req, res) => {
-    console.log(req.params.id);
-    let quiz = quizzes.find((q) => q.id === parseInt(req.params.id));
-    if (quiz) {
-        res.status(200).json({
-            done: true,
-            result: quiz,
-            message: "Quiz retrieved successfully.",
-        });
-    } else {
-        res.status(200).json({
-            done: false,
-            result: undefined,
-            message: `No quiz with id ${req.params.id} found.`,
-        });
-    }
-});
+// Determines which port to listen to.
+const PORT = process.env.PORT || 3000;
 
-app.post("/score", (req, res) => {
-    const score = {
-        quizTaker: req.body.quizTaker,
-        quizName: req.body.quizName,
-        score: req.body.score,
-    };
-    scores.push(score);
-    res.status(200).json({ done: true, message: "Score posted successfully" });
+// Starts the express server and sets a callback function for when startup finishes
+application.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
 });
-
-app.get("/scores/:quiztaker/:quizname", (req, res) => {
-    let userScores = [];
-    for (let score of scores) {
-        if (
-            score.quizTaker === req.params.quiztaker &&
-            score.quizName === req.params.quizname
-        ) {
-            userScores.push(score.score);
-        }
-    }
-    if (userScores.length > 0) {
-        res.status(200).json({
-            done: true,
-            result: userScores,
-            message: "Found scores sucessfully.",
-        });
-    }
-    res.status(404).json({
-        done: false,
-        result: [],
-        message: "No score found.",
-    });
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}.`));
